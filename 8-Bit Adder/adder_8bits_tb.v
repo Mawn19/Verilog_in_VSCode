e97ec549b9ee56c983d45c9f9b3dd0c3d0503d8a
Mawn19 <87337070+Mawn19@users.noreply.github.com>
2024-05-23 01:28:19 +0200

    8 bits adder has been simulated again. Simulation issues fixed

--- 8-Bit Adder/adder_8bits_tb.v
+++ 8-Bit Adder/adder_8bits_tb.v
@@ -25,15 +25,18 @@ adder_8bits uut(
            full_a = 8'b0000_0001 ; full_b = 8'b0000_0000; full_c_in = 8'b0000_0000; // 1
         #1 full_a = 8'b0000_0000 ; full_b = 8'b0000_0001; full_c_in = 8'b0000_0000; // 1
         #1 full_a = 8'b0000_0001 ; full_b = 8'b0000_0010; full_c_in = 8'b0000_0000; // 3
-        #1 full_a = 8'b0000_1000 ; full_b = 8'b0000_0001; full_c_in = 8'b0000_0000;// 9
-        #1 full_a = 8'b0000_0010 ; full_b = 8'b0000_0010; full_c_in = 8'b0000_0100; // 2
-        #1 full_a = 8'b1000_0000 ; full_b = 8'b1000_0000; full_c_in = 8'b0000_0001; // 256 // to be checked
+        // #1 full_a = 8'b0000_1000 ; full_b = 8'b0000_0001; full_c_in = 8'b0000_0000;// 9
+        // #1 full_a = 8'b0000_0010 ; full_b = 8'b0000_0010; full_c_in = 8'b0000_0100; // 2
+        // #1 full_a = 8'b1000_0000 ; full_b = 8'b1000_0000; full_c_in = 8'b0000_0001; // 256 // to be checked
         
-        #1 full_a = 8'b1111_1111 ; full_b = 8'b0000_0001; full_c_in = 8'b1111_1110; // 256
+        #1 full_a = 8'b1111_1110 ; full_b = 8'b0000_0001; full_c_in = 8'b0000_0000; // 255
+        #1 full_a = 8'b1111_1111 ; full_b = 8'b0000_0001; full_c_in = 8'b1111_1110; //255 + 1 overflow sum 0 carry_out 1
 
-        #1 full_a = 8'b0000_0001 ; full_b = 8'b0000_0001; full_c_in = 8'b0000_0010; // 2
-        #1 full_a = 8'b0000_1111 ; full_b = 8'b0000_0001; full_c_in = 8'b0001_1110; // 16
-        #1 full_a = 8'b0000_1111 ; full_b = 8'b0000_0011; full_c_in = 8'b0001_1110; // 18
+        // #1 full_a = 8'b1111_1111 ; full_b = 8'b0000_0001; full_c_in = 8'b1111_1110; // 256
+
+        // #1 full_a = 8'b0000_0001 ; full_b = 8'b0000_0001; full_c_in = 8'b0000_0010; // 2
+        // #1 full_a = 8'b0000_1111 ; full_b = 8'b0000_0001; full_c_in = 8'b0001_1110; // 16
+        // #1 full_a = 8'b0000_1111 ; full_b = 8'b0000_0011; full_c_in = 8'b0001_1110; // 18
 
         // #1 full_a = 8'b0000_0000 ; full_b = 8'b0000_0000; full_c_in = 8'b0000_0000; 
         // #1 full_a = 8'b0000_0000 ; full_b = 8'b0000_0000; full_c_in = 8'b0000_0000; 
@@ -44,7 +47,7 @@ adder_8bits uut(
       $display("Test complete");
     end
 
-  initial #10 $finish;
+  initial #12 $finish;
 
 
 endmodule
