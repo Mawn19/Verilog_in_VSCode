a27eee26d2459c5a0723b501b0f00863b8a0778b
Mawn19 <87337070+Mawn19@users.noreply.github.com>
2024-05-22 23:43:44 +0200

    new changes in 8-bit adder

--- 8-Bit Adder/adder_8bits.v
+++ 8-Bit Adder/adder_8bits.v
@@ -17,12 +17,15 @@ full_adder FA_block5 (.A(first_byte[5]), .B(second_byte[5]), .C_in(carry_in[5]),
 full_adder FA_block6 (.A(first_byte[6]), .B(second_byte[6]), .C_in(carry_in[6]), .Sum(sum_bytes[6]), .C_out(carry_out[6]));
 full_adder FA_block7 (.A(first_byte[7]), .B(second_byte[7]), .C_in(carry_in[7]), .Sum(sum_bytes[7]), .C_out(carry_out[7]));
 
-assign carry_in[1] = carry_in[0];
-assign carry_in[2] = carry_in[1];
-assign carry_in[3] = carry_in[2];
-assign carry_in[4] = carry_in[3];
-assign carry_in[5] = carry_in[4];
-assign carry_in[6] = carry_in[5];
-assign carry_in[7] = carry_in[6];
+//assign carry_in[0] = ?
+
+assign carry_in[1] = carry_out[0];
+assign carry_in[2] = carry_out[1];
+assign carry_in[3] = carry_out[2];
+assign carry_in[4] = carry_out[3];
+assign carry_in[5] = carry_out[4];
+assign carry_in[6] = carry_out[5];
+assign carry_in[7] = carry_out[6];
+
 
 endmodule
